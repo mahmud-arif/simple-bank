e4aa86bcae50ef314b2c6fd5c214dc9c11bf9f41
arif mahmud <mahmudarif1175@gmail.com>
2025-08-26 22:42:39 +0600

    fixed: migration file with db creation

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://admin:secret@localhost:5432/mydb?sslmode=disable"
+	dbSource = "postgresql://admin:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
